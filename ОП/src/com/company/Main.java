3652e91cbadb7f541564f7131b7365d3a9f4f466
Vitalik1754 <99317811+Vitalik1754@users.noreply.github.com>
2022-04-12 20:11:21 +0300

    Add files via upload

---
+++ ОП/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code her
+
+    System.out.println("Батько: Носаль Мар'ян Ігорович, 1981 року народження , пенсіонер");
+        System.out.println("Мати : Носаль Марія Ярославівна, 1985 року народження , підприємець");
+        System.out.println("Брат : Носаль Захар Мар'янович, 2009 року народження , школяр");
+
+
+
+
+
+    }
+}
